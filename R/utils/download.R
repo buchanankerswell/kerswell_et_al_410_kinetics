9fdcb6faca816d6a68014e5b9739e461028716f9
Buchanan Kerswell <buck.kerswell@gmail.com>
2025-11-25 16:29:18 +0000

    Fix osfr download

--- R/utils/download.R
+++ R/utils/download.R
@@ -4,7 +4,6 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 download_simulation_results_from_osf <- function(out_dir) {
   osf_retrieve_node("9phwc") |>
-    osf_ls_files(project, pattern = "results") |>
-    osf_download(path = out_dir, recurse = TRUE, conflicts = "overwrite", progress = TRUE)
-
+    osf_ls_files(pattern = "results") |>
+    osf_download(path = out_dir, recurse = TRUE, conflicts = "skip", progress = TRUE, verbose = TRUE)
 }
